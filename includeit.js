00715f4987dbb7b0256889e238ca8260d4b1cce9
Maximilian Schmahl <mxschmhl@MacBookAir.fritz.box>
2025-11-17 19:25:52 +0100

    add icon

--- includeit.js
+++
@@ -1,8 +0,0 @@
-function includeit(id, file, callback) {
-  fetch(file)
-    .then(r => r.text())
-    .then(t => {
-      document.getElementById(id).innerHTML = t;
-      if (callback) callback();
-    });
-}
